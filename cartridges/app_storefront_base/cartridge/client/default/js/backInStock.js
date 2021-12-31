1cf5ffb8ad8f633bf6a9501152d7c5a8e5f9c5f1
lyubohar <lyubohar@gmail.com>
2021-12-31 09:14:21 +0200

    Ajax js structure update

--- cartridges/app_storefront_base/cartridge/client/default/js/backInStock.js
+++ cartridges/app_storefront_base/cartridge/client/default/js/backInStock.js
@@ -3,5 +3,5 @@
 var processInclude = require('./util');
 
 $(document).ready(function () {
-    processInclude(require('./product/detail'));
+    processInclude(require('./product/backInStock'));
 });
