d72b00f980f9a936a8dd3b347e7671b53a08d8a9
lyubohar <lyubohar@gmail.com>
2021-12-31 12:57:09 +0200

    Update TwilioServiceCall.js

--- cartridges/plugin_backinstock/cartridge/scripts/steps/TwilioServiceCall.js
+++ cartridges/plugin_backinstock/cartridge/scripts/steps/TwilioServiceCall.js
@@ -14,44 +14,42 @@ module.exports.execute = function () {
         var currentObjectProductId = currentObject.getCustom().productId;
         var currentProduct = ProductMgr.getProduct(currentObjectProductId);
         var currentProductAvailability = currentProduct.getAvailabilityModel().isInStock();
+        
+        if (currentProductAvailability === true) {
+            
+            // Twilio service call
+    
+            // function backToStockService() {
+    
+            //     var localServiceRegistry = dw.svc.LocalServiceRegistry
+            //     var getTwilioService = localServiceRegistry.createService("Twilio", {
+            
+            //         createRequest: function(svc, args) {
+            //             svc.setRequestMethod('POST');
+            //             return args;
+            //         },
+            
+            //         parseResponse: function(svc, client) {
+            //             return client.text;
+            //         }
+            
+            //     });
+            
+            //     var response = getTwilioService.call().object;
+            //     return response;
+              
+            // };
+            
+            // Delete custom objects
+    
+            try {
+                Transaction.wrap(function () {
+                    CustomObjectMgr.remove(currentObject);
+                });            
+            } catch (error) {
+                error = true;
+            }
+        }    
     }
-    return currentProductAvailability;
 
-    // currentProductAvailability = false;
-
-    if (currentProductAvailability === true) {
-        
-        // Twilio service call
-
-        // function backToStockService() {
-
-        //     var localServiceRegistry = dw.svc.LocalServiceRegistry
-        //     var getTwilioService = localServiceRegistry.createService("Twilio", {
-        
-        //         createRequest: function(svc, args) {
-        //             svc.setRequestMethod('POST');
-        //             return args;
-        //         },
-        
-        //         parseResponse: function(svc, client) {
-        //             return client.text;
-        //         }
-        
-        //     });
-        
-        //     var response = getTwilioService.call().object;
-        //     return response;
-          
-        // };
-        
-        // Delete custom objects
-
-        try {
-            Transaction.wrap(function () {
-                CustomObjectMgr.remove(currentObject);
-            });            
-        } catch (error) {
-            error = true;
-        }
-    }    
 }
