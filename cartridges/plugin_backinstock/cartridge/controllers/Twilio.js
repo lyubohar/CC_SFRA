8bfe8cf040a476336700c5d28a035a9f0e705ffa
lyubohar <lyubohar@gmail.com>
2022-01-06 08:21:16 +0200

    Update Twilio.js

--- cartridges/plugin_backinstock/cartridge/controllers/Twilio.js
+++ cartridges/plugin_backinstock/cartridge/controllers/Twilio.js
@@ -24,87 +24,71 @@ server.post('Subscribe',
         var Resource = require('dw/web/Resource');
         var Transaction = require('dw/system/Transaction');
         var CustomObjectMgr = require('dw/object/CustomObjectMgr');
-
+        var error = false;
+        
         // Take form values from Ajax
 
         var formAjax = req.form;
         var formProduct = formAjax.product;
         var formPhone = formAjax.phone;
+        // if (!formProduct) {
+        //     res.json({
+        //         error: true,
+        //         msg: Resource.msg('message.backInStock.error', 'common', null)
+        //     });            
+        // }; 
+      
+        // Iterate all objects to see if any object exists for current product
+        
+        var type = 'NotifyMeBackInStock';
+        var keyValue = formProduct;
+        var allObjects = CustomObjectMgr.getAllCustomObjects(type); 
+        var currentObject = CustomObjectMgr.getCustomObject(type, keyValue);
 
-        if (!formAjax || !formProduct) {
+        while (allObjects.hasNext()) {  
+            var currentObjectProductId = allObjects.next().getCustom().productId;  
+            
+            if (currentObjectProductId === formProduct) {                          
+                var currentObjectPhoneNumbers = currentObject.getCustom().phoneNumbers; 
+                var mergedPhoneNumbers = currentObjectPhoneNumbers + "," + formPhone;       // Merge new with existing
+                var filteredPhoneNumbers = Array.from(new Set(mergedPhoneNumbers.split(','))).toString(); // Remove duplicates
+                
+                try {
+                    Transaction.wrap(function() {
+                        currentObject.custom.phoneNumbers = filteredPhoneNumbers;       // Store old data merged with new data
+                    });
+                } catch (error) {
+                    res.json({
+                        error: true,
+                        msg: Resource.msg('message.backInStock.error', 'common', null)
+                    });
+                } finally {
+                    res.json({
+                        success: true,
+                        msg: Resource.msg('message.backInStock.success', 'common', null)
+                    }); 
+                }; 
+            }    
+        }
+
+        // Create new object if none exists
+
+        try {
+            Transaction.wrap(function() {
+                var backInStockObject = CustomObjectMgr.createCustomObject(type, keyValue);
+                backInStockObject.custom.phoneNumbers = formPhone;
+            });
+        } catch (error) {
             res.json({
                 error: true,
                 msg: Resource.msg('message.backInStock.error', 'common', null)
             });
-        } else if (!formPhone) {
-            res.json({
-                error: true,
-                msg: Resource.msg('message.backInStock.missing', 'common', null)
-            });
-        } else {
+        } finally {
             res.json({
                 success: true,
                 msg: Resource.msg('message.backInStock.success', 'common', null)
-            });           
-        }
-
-        // Store transaction as a reusable function
-        
-        var type = 'NotifyMeBackInStock';
-        var keyValue = formProduct;
-
-        var transaction = () => {                                           
-            Transaction.wrap(function() {
-                var backInStockObject = CustomObjectMgr.createCustomObject(type, keyValue);
-
-                backInStockObject.custom.phoneNumbers = formPhone;
-            });
-        }
-        
-        // Proceed if custom objects already exist
-
-        var allObjects = CustomObjectMgr.getAllCustomObjects(type); 
-        var currentObject = CustomObjectMgr.getCustomObject(type, keyValue);
-        var error = false;
-
-        while (allObjects.hasNext()) {  
-
-            // Iterate all objects to see if any object exists for current product
-            var currentObjectProductId = allObjects.next().getCustom().productId;  
-            
-            // If yes
-            if (currentObjectProductId === formProduct) {                          
-                var currentObjectPhoneNumbers = currentObject.getCustom().phoneNumbers; 
-                var mergedPhoneNumbers = currentObjectPhoneNumbers + "," + formPhone; // Merge new with existing
-                var filteredPhoneNumbers = Array.from(new Set(mergedPhoneNumbers.split(','))).toString(); // Remove duplicates
-
-                try {
-                    Transaction.wrap(function() {
-
-                        // Store old data merged with new data
-                        currentObject.custom.phoneNumbers = filteredPhoneNumbers; 
-                    });
-                } catch (error) {
-                    error = true;
-                };  
-
-                // If no, just store new data
-                } else {                                                          
-                try {
-                    transaction();
-                } catch (error) {
-                    error = true;
-                };  
-            }            
-        }
-
-        // Create new custom object if none exists
-
-        try {
-            transaction();
-        } catch (error) {
-            error = true;
-        };         
+            }); 
+        } 
         
         return next();
 });
