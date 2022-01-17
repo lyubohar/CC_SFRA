f22e6d84cd10c29a8ac1a4f7c9f9d6bb972c3be1
lyubohar <lyubohar@gmail.com>
2022-01-17 19:34:44 +0200

    refactor twilio service

--- cartridges/plugin_backinstock/cartridge/scripts/steps/serviceCallAndDelete.js
+++ cartridges/plugin_backinstock/cartridge/scripts/steps/serviceCallAndDelete.js
@@ -1,20 +1,18 @@
 /**
  * Query all custom objects with id = 'NotifyMeBackInStock'. Loop them and check the product availability. If the product is back in stock, make a service call to Twilio API for all phone numbers stored in that custom objects. When process finished successfully, delete the custom object.
- * Handles Twilio service. Sends request as body (x-www-form-urlencoded format) and gets response.
- * @returns {String} - returns string with data to be sent to the Twilio API for processing.
 */
 
-var Resource = require('dw/web/Resource');
 var CustomObjectMgr = require('dw/object/CustomObjectMgr');
 var ProductMgr = require('dw/catalog/ProductMgr');
 var Transaction = require('dw/system/Transaction');
+var twilioService = require('~/cartridge/scripts/twilioService.js');
 
 module.exports.execute = function () {
     var type = 'NotifyMeBackInStock';
     var allObjects = CustomObjectMgr.getAllCustomObjects(type);
     var error = false;
 
-// Iterate through objects
+    // Iterate through objects
     
     while (allObjects !== null && allObjects.hasNext()) {                   
         var currentObject = allObjects.next();
@@ -24,41 +22,18 @@ module.exports.execute = function () {
         var phonesArray = currentObject.getCustom().phoneNumbers.split(',');
 
         if (currentProductAvailability === true) {                          // Check for product availability
+
             phonesArray.forEach(function (phoneTo) {                        // Iterate through phone numbers
-                function backInStockService() {                             // Twilio service call
-                    var localServiceRegistry = dw.svc.LocalServiceRegistry;
-                    var smsTwilioService = localServiceRegistry.createService("plugin_backinstock.http.twilio.sms", {
-        
-                        createRequest: function(svc) {
-                            svc.addHeader('Content-Type', 'application/x-www-form-urlencoded');
-                            var phoneFrom = '13048496496';
-                            var smsBody = Resource.msg('message.backInStock.bodystart', 'common', null) + currentProduct.name + Resource.msg('message.backInStock.bodyend', 'common', null);
-                            var requestString = 'To=%2B' + phoneTo + '&From=%2B' + phoneFrom + '&Body=' + smsBody;
-        
-                            return requestString;
-                        },
-        
-                        parseResponse: function(response) {
-                            return response;
-                        }
-                
-                    });
-                    return smsTwilioService.call();
-                };
-                backInStockService();
-            }, deleteCustomObjects());
+                twilioService.backInStockService(currentProduct, phoneTo);
+            });
+
+            try {                                                           // Delete custom objects
+                Transaction.wrap(function () {
+                    CustomObjectMgr.remove(currentObject);
+                });            
+            } catch (error) {
+                error = true;
+            }
         }    
-    }
-    
-// Delete custom objects
-            
-    function deleteCustomObjects() {
-        try {
-            Transaction.wrap(function () {
-                CustomObjectMgr.remove(currentObject);
-            });            
-        } catch (error) {
-            error = true;
-        }
-    }    
+    }   
 }
---
+++ cartridges/plugin_backinstock/cartridge/scripts/twilioService.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * Handles Twilio service. Sends request as body (x-www-form-urlencoded format) and gets response.
+ * @returns {String} - returns string with data to be sent to the Twilio API for processing.
+*/
+
+var Resource = require('dw/web/Resource');
+
+function backInStockService(argCurrentProduct, argPhoneTo) {                             
+    var localServiceRegistry = dw.svc.LocalServiceRegistry;
+    var smsTwilioService = localServiceRegistry.createService("plugin_backinstock.http.twilio.sms", {
+    
+        createRequest: function(svc) {
+            svc.addHeader('Content-Type', 'application/x-www-form-urlencoded');
+            var phoneFrom = '13048496496';
+            var smsBody = Resource.msg('message.backInStock.bodystart', 'common', null) + argCurrentProduct.name + Resource.msg('message.backInStock.bodyend', 'common', null);
+            var requestString = 'To=%2B' + argPhoneTo + '&From=%2B' + phoneFrom + '&Body=' + smsBody;
+
+            return requestString;
+        },
+
+        parseResponse: function(response) {
+            return response;
+        }
+
+    });
+    return smsTwilioService.call();
+};
+
+exports.backInStockService = backInStockService;
