688eb06e041f007af6c364381303245a07dffb82
Lyubomir Harizanov <lyubohar@gmail.com>
2022-03-22 17:28:06 +0200

    Exercise: Content Assets and Slots: 1. and 2.

--- cartridges/app_storefront_homework_playground/cartridge/client/default/js/lyubo.js
+++ cartridges/app_storefront_homework_playground/cartridge/client/default/js/lyubo.js
@@ -1,2 +1,11 @@
-
-console.log('test4')
+$(document).ready(function(){
+    $('.lyubo-hero-carousel').slick({
+        arrows: true,
+        speed: 400,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    }); 
+});
