860a9aa4f734e28ffff99758c80650f211ce63c7
Lyubomir Harizanov <lyubohar@gmail.com>
2022-03-28 16:31:31 +0300

    Exercises: Page designer

--- cartridges/app_storefront_homework_playground/cartridge/controllers/Home.js
+++ cartridges/app_storefront_homework_playground/cartridge/controllers/Home.js
@@ -22,18 +22,29 @@ server.append('Show', function(req, res, next) {
 
     var viewData = res.getViewData();
     
-    // Append inline
+    // Append inline exercise
     if (!viewData) {
         next();
     } else {
         viewData.some_dummy_key = 'Lyubo append inline test'
     }
 
-    // Append by hook
+    // PageDesigner exercise
+    var PageMgr = require('dw/experience/PageMgr');
+    var Site = require('dw/system/Site');
+    var pageDesignerPageName = Site.getCurrent().getPreferences().getCustom()['pd_pageID'];
+    var page = PageMgr.getPage(pageDesignerPageName);
+
+    if (page && page.isVisible()) {
+        res.page(pageDesignerPageName);
+    } else {
+        res.render('home/homePage');
+    }
+
+    // Append by hook exercise
     if (HookMgr.hasHook('app.playground.dummy.hook')) {
         viewData = HookMgr.callHook('app.playground.dummy.hook', 'dummyHookFunction');       
     }
-
     res.setViewData(viewData);
     next();
 });
---
+++ cartridges/app_storefront_homework_playground/cartridge/experience/components/commerce_assets/lyubo_custom_hero.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var Template = require('dw/util/Template');
+var HashMap = require('dw/util/HashMap');
+var ImageTransformation = require('*/cartridge/experience/utilities/ImageTransformation.js');
+
+
+/**
+ * Render logic for storefront.imageAndText component.
+ * @param {dw.experience.ComponentScriptContext} context The Component script context object.
+ * @param {dw.util.Map} [modelIn] Additional model values created by another cartridge. This will not be passed in by Commcerce Cloud Plattform.
+ *
+ * @returns {string} The markup to be displayed
+ */
+module.exports.render = function (context, modelIn) {
+    var model = modelIn || new HashMap();
+    var content = context.content;
+
+    model.title = content.title ? content.title : null;
+    model.image = ImageTransformation.getScaledImage(content.image);
+    model.link = content.ITCLink ? content.ITCLink : '#';
+    model.alt = content.alt ? content.alt : null;
+    model.descr = content.descr ? content.descr : null;
+    model.cta = content.cta ? content.cta : null;
+
+    return new Template('experience/components/commerce_assets/lyubo_custom_hero').render(model).text;
+};
