6b2bdbf159c6f9ef0aab5bf2072cffed4467499b
Lyubomir Harizanov <lyubohar@gmail.com>
2022-03-30 08:17:12 +0300

    Update confirmationEmail.js

--- cartridges/app_storefront_homework_playground/cartridge/scripts/helpers/confirmationEmail.js
+++ cartridges/app_storefront_homework_playground/cartridge/scripts/helpers/confirmationEmail.js
@@ -3,6 +3,6 @@
 var Template = require('dw/util/Template'); 
 
 module.exports.render = function (productId) {
-    return new Template('templates/default/checkout/confirmation/confirmationEmailIsInclude').render(productId).text
+    return new Template('checkout/confirmation/confirmationEmailIsInclude').render(productId).text
 };
 
