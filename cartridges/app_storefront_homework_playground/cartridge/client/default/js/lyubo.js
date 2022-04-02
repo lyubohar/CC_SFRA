75b5846f2889ac52c17b5c46a295760cb492e346
Lyubomir Harizanov <lyubohar@gmail.com>
2022-04-02 18:41:31 +0300

    Exercise carousel
    Dynamic settings support

--- cartridges/app_storefront_homework_playground/cartridge/client/default/js/lyubo.js
+++ cartridges/app_storefront_homework_playground/cartridge/client/default/js/lyubo.js
@@ -1,51 +1,62 @@
+var breakpoints = require('./settings/breakpoints.json');
+
 $(document).ready(function(){
-    var slickElement = $('.lyubo-hero-carousel')
+    var carouselElement = $('.lyubo-hero-carousel')
 
-    var xsDisplayIndicators = $(slickElement).attr('data-xs-indicators')
-    var xsDisplayControls = $(slickElement).attr('data-xs-controls')
-    var xsSlidesDisplay = $(slickElement).attr('data-xs-slides-display')
-    var xsSlidesScroll = $(slickElement).attr('data-xs-slides-scroll')
+    var xsDisplayIndicators = $(carouselElement).attr('data-xs-indicators') === 'true' ? true : false
+    var xsDisplayControls = $(carouselElement).attr('data-xs-controls') === 'true' ? true : false
+    var xsSlidesDisplay = $(carouselElement).attr('data-xs-slides-display') ? Number($(carouselElement).attr('data-xs-slides-display')) : 1
+    var xsSlidesScroll = $(carouselElement).attr('data-xs-slides-scroll') ? Number($(carouselElement).attr('data-xs-slides-scroll')) : 1
 
-    var smDisplayIndicators = $(slickElement).attr('data-sm-indicators')
-    var smDisplayControls = $(slickElement).attr('data-sm-controls')
-    var smSlidesDisplay = $(slickElement).attr('data-sm-slides-display')
-    var smSlidesScroll = $(slickElement).attr('data-sm-slides-scroll')
+    var smDisplayIndicators = $(carouselElement).attr('data-sm-indicators') === 'true' ? true : false
+    var smDisplayControls = $(carouselElement).attr('data-sm-controls') === 'true' ? true : false
+    var smSlidesDisplay = $(carouselElement).attr('data-sm-slides-display') ? Number($(carouselElement).attr('data-sm-slides-display')) : 2
+    var smSlidesScroll = $(carouselElement).attr('data-sm-slides-scroll') ? Number($(carouselElement).attr('data-sm-slides-scroll')) : 1
 
-    var mdDisplayIndicators = $(slickElement).attr('data-md-indicators')
-    var mdDisplayControls = $(slickElement).attr('data-md-controls')
-    var mdSlidesDisplay = $(slickElement).attr('data-md-slides-display')
-    var mdSlidesScroll = $(slickElement).attr('data-md-slides-scroll')
-
-    var currentWidth = $(window).width()
+    var mdDisplayIndicators = $(carouselElement).attr('data-md-indicators') === 'true' ? true : false
+    var mdDisplayControls = $(carouselElement).attr('data-md-controls') === 'true' ? true : false
+    var mdSlidesDisplay = $(carouselElement).attr('data-md-slides-display') ? Number($(carouselElement).attr('data-md-slides-display')) : 3
+    var mdSlidesScroll = $(carouselElement).attr('data-md-slides-scroll') ? Number($(carouselElement).attr('data-md-slides-scroll')) : 1
     
+    if ($(window).width() < breakpoints.mobile) {
+        var displayIndicators = xsDisplayIndicators
+        var displayControls = xsDisplayControls
+        var slidesDisplay = xsSlidesDisplay
+        var slidesScroll = xsSlidesScroll    
+    } else if ($(window).width() < breakpoints.tablet) {
+        var displayIndicators = smDisplayIndicators
+        var displayControls = smDisplayControls
+        var slidesDisplay = smSlidesDisplay
+        var slidesScroll = smSlidesScroll    
+    } else {
+        var displayIndicators = mdDisplayIndicators
+        var displayControls = mdDisplayControls
+        var slidesDisplay = mdSlidesDisplay
+        var slidesScroll = mdSlidesScroll
+    }  
+    
+    function slickInit() {
+        carouselElement.slick({
+            arrows: displayIndicators,
+            dots: displayControls,
+            slidesToShow: slidesDisplay,
+            slidesToScroll: slidesScroll,
+            speed: 400,
+            infinite: true
+        });
+    }
+    slickInit()
+
     $(window).resize(function() {
-        var dynamicWidth = $(window).width();
-        
-        if (currentWidth < 544 || dynamicWidth < 544) {
-            var prefix = 'xs'
-        } else if (currentWidth < 992 || dynamicWidth < 992) {
-            var prefix = 'sm'
-        } else {
-            var prefix = 'md'
-        }
-        console.log(prefix)
-    }).resize();
-
-
-    console.log(mdSlidesScroll)
-    var column = parseInt(mdSlidesScroll)
-    var test = parseInt(mdDisplayIndicators)
-
-    slickElement.slick({
-        arrows: test,
-        dots: mdDisplayControls,
-        slidesToShow: mdSlidesDisplay,
-        slidesToScroll: 1,
-        speed: 400,
-        infinite: true,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        mobileFirst: true
-    });
+        // var prefix 
+        // if ($(window).width() < breakpoints.mobile) {
+        //     prefix = 'xs'
+        // } else if ($(window).width() < breakpoints.tablet) {
+        //     prefix = 'sm'
+        // } else {
+        //     prefix = 'md'
+        // }
+        // carouselElement.slick("unslick")
+        // slickInit()
+    }).resize()
 });
-
