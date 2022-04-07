4d4a46186658b71ccf6ee0a87ae8c95d31d4a642
Lyubomir Harizanov <lyubohar@gmail.com>
2022-04-07 11:06:28 +0300

    Exercise PDP

--- cartridges/app_storefront_homework_playground/cartridge/client/default/js/product/base.js
+++ cartridges/app_storefront_homework_playground/cartridge/client/default/js/product/base.js
@@ -1,3 +1,28 @@
 'use strict';
 
+var base = require('base/product/base');
+
+$(document).ready(function () {
+    var imageElement = $('.carousel-item img')
+
+    $(imageElement).click(function () {
+        var currentImageUrl = $(this).attr('src')
+        $('.carousel-item-main img').attr('src', currentImageUrl)
+    });
+
+});
+
+module.exports = {
+    selectAttribute: function () {
+        $(document).on('change', '.attr-element', function (e) {
+            e.preventDefault();
+
+            var $productContainer = $(this).closest('.set-item');
+            if (!$productContainer.length) {
+                $productContainer = $(this).closest('.product-detail');
+            }
+            base.attributeSelect(e.currentTarget.value, $productContainer);
+        });
+    }
+};
 
