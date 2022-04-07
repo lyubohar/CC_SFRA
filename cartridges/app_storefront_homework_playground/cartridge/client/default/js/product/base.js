ceec9030dd635ae6f98cea454cef832a77197d2c
Lyubomir Harizanov <lyubohar@gmail.com>
2022-04-07 14:17:47 +0300

    Update base.js

--- cartridges/app_storefront_homework_playground/cartridge/client/default/js/product/base.js
+++ cartridges/app_storefront_homework_playground/cartridge/client/default/js/product/base.js
@@ -2,27 +2,23 @@
 
 var base = require('base/product/base');
 
-$(document).ready(function () {
-    var imageElement = $('.carousel-item img')
+// Set active thumbnail 
+$('.carousel-item').click(function () {
+    $('.carousel-item').removeClass("active");
+    $(this).addClass("active");  
+});  
 
-    // Set active thumbnail 
-    $('.carousel-item').click(function () {
-        $('.carousel-item').removeClass("active");
-        $(this).addClass("active");  
-    });  
+// Show large image on click of thumbnail
+$('.carousel-item img').click(function () {
+    var smallImageUrl = $(this).attr('src')
+    var largeImageUrl = smallImageUrl.replace('small', 'large');
+    $('.carousel-item-main img').attr('src', largeImageUrl)
+});
 
-    // Show large image on click of thumbnail
-    $(imageElement).click(function () {
-        var smallImageUrl = $(this).attr('src')
-        var largeImageUrl = smallImageUrl.replace('small', 'large');
-        $('.carousel-item-main img').attr('src', largeImageUrl)
-    });
-
-    // Set active attribute
-    $('.attr-element').click(function () {
-        $('.attr-element').removeClass("active");
-        $(this).addClass("active");  
-    });
+// Set active attribute
+$('.attr-element').click(function () {
+    $('.attr-element').removeClass("active");
+    $(this).addClass("active");  
 });
 
 module.exports = {
